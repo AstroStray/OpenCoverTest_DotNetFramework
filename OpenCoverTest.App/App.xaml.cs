0bf2d299f0b7197a8a66927a956f67543687c480
KimYoungJin <oz.kim@apr-in.com>
2025-09-01 11:15:10 +0900

    # Add  - Test Classes, Methods

--- OpenCoverTest.App/App.xaml.cs
+++ OpenCoverTest.App/App.xaml.cs
@@ -14,4 +14,24 @@ namespace OpenCoverTest.App
     public partial class App : Application
     {
     }
+
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+    }
+
+    public class StringHelper
+    {
+        public string Reverse(string input)
+        {
+            if (input == null) return null;
+            return new string(input.Reverse().ToArray());
+        }
+    }
+
+    public class MathUtils
+    {
+        public bool IsEven(int number) => number % 2 == 0;
+    }
 }
--- OpenCoverTest.Tests/Class1.cs
+++ OpenCoverTest.Tests/Class1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using OpenCoverTest.App;
 
 namespace OpenCoverTest.Tests
 {
@@ -13,6 +14,40 @@ namespace OpenCoverTest.Tests
         public void TestMethod()
         {
             Assert.True(true);
+        }
+    }
+
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsSum()
+        {
+            var calc = new Calculator();
+            Assert.Equal(5, calc.Add(2, 3));
+        }
+
+        [Fact]
+        public void Subtract_ReturnsDifference()
+        {
+            var calc = new Calculator();
+            Assert.Equal(1, calc.Subtract(3, 2));
+        }
+    }
+
+    public class StringHelperTests
+    {
+        [Fact]
+        public void Reverse_ReturnsReversedString()
+        {
+            var helper = new StringHelper();
+            Assert.Equal("cba", helper.Reverse("abc"));
+        }
+
+        [Fact]
+        public void Reverse_NullInput_ReturnsNull()
+        {
+            var helper = new StringHelper();
+            Assert.Null(helper.Reverse(null));
         }
     }
 }
