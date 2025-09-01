54dc4ca094fd072fda2cdd625d2b83b85c06292d
KimYoungJin <oz.kim@apr-in.com>
2025-09-01 14:43:57 +0900

    asdgdsaf

--- OpenCoverTest.Tests/Class1.cs
+++ OpenCoverTest.Tests/Class1.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenCoverTest.Core;
 using Xunit;
-using OpenCoverTest.App;
 
 namespace OpenCoverTest.Tests
 {
